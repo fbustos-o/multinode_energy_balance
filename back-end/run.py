e6fd64a566a8da4ae57fa682b597c198c8d69400
Fabián Bustos <121578605+fbustos-o@users.noreply.github.com>
2026-06-04 16:32:12 +0900

    Delete back-end/run.py

--- back-end/run.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-if __name__ == "__main__":
-    # We point to the FastAPI instance 'app' located in api/main.py
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
